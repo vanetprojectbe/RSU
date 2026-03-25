c6c75c55a898dd1b220e119af6326d43ccada02d
vanet project <vanetprojectbe@gmail.com>
2026-03-25 10:50:31 +0530

    Delete src directory

--- src/sd_queue.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void sdq_init();
-void sdq_enqueue(const char* json);
-void sdq_retry();
-uint32_t sdq_pending_count();
--- src/sim800.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-bool sim800_init();
-bool sim800_is_registered();
-bool sim800_http_post(const char* path, const char* json);
-bool sim800_http_get(const char* path, String& out);
